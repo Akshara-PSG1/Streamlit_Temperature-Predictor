2ca96133f058e4ace0f4cd9ba7fbdce580e166e9
Akshara-PSG1 <122364337+Akshara-PSG1@users.noreply.github.com>
2024-08-27 15:43:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+
 
 from tensorflow.keras.models import load_model
 
